dc0385f858cd22df69840960db406cc8ddbb8b81
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2023-07-22 17:33:59 -0300

    Create database seed

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin')
+        ])->assignRole('admin');
+        User::create([
+            'name' => 'Manager',
+            'email' => 'manager@gmail.com',
+            'password' => bcrypt('manager')
+        ])->assignRole('admin');
+        User::create([
+            'name' => 'worker',
+            'email' => 'worker@gmail.com',
+            'password' => bcrypt('worker')
+        ])->assignRole('worker');
+    }
+}
